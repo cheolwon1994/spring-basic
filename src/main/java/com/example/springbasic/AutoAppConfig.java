eab1b2922e9d51523024f824b4901ed86eb564ca
cheolwon1994 <chlcjfahs@naver.com>
2023-07-24 13:42:39 +0900

    generate test codes for bean duplicate

---
+++ src/main/java/com/example/springbasic/AutoAppConfig.java
@@ -0,0 +1,20 @@
+package com.example.springbasic;
+
+import com.example.springbasic.member.MemberRepository;
+import com.example.springbasic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+
+    }
+}
